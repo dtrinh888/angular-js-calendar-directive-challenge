9f71d89818c78c9cf5869221e4dd2cd44be83177
dtrinh888 <dtrinh888@gmail.com>
2016-01-31 21:50:18 -0800

    3 of 6 testing units successful

--- app/app.js
+++ app/app.js
@@ -5,16 +5,15 @@ angular.module('calendarDemoApp', [])
 		var day = today.getDay();
 		var month = today.getMonth();
 		var year = today.getFullYear();
-		console.log('day', day);
 		$scope.date = {
 			day: day,
 			month: month,
 			year: year
 		};
-		/*console.log('??',$scope.date);*/
+
 
 		$scope.today = today.toDateString();
-		/*console.log('today', $scope.today);*/
+
 
 		$scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
 						'Thursday', 'Friday', 'Saturday'];
@@ -29,27 +28,20 @@ angular.module('calendarDemoApp', [])
 			years.push(i);
 		}
 		$scope.years = years;
+		console.log('years', $scope.years[40]);
 
 		$scope.$watch('date', function(newDate){
-			console.log('newdate', newDate);
+			console.log('newdate',new Date(newDate.year, newDate.month, 1));
 			var range = CalendarRange.getMonthlyRange(new Date(newDate.year, newDate.month, 1));
-			/*console.log('range',range);*/
 			var totalWeeks = range.days.length/7;			
 			var weeks = [];
 			for(i=0; i<totalWeeks; i++){
 				weeks[i] = [];
-				/*console.log('weeks', weeks[5]);*/
-				/*console.log('totalWeeks', totalWeeks);*/
 				for(j=0; j<7; j++){
-					/*console.log('days', j+(i*7));*/
 					weeks[i].push(range.days[j+(i*7)]);
-					/*console.log('j',j);
-					console.log('weeks[i]', weeks[0]);*/
 				}
 			}
 			$scope.weeks = weeks;
-			console.log('scope.weeks',$scope.weeks[0][5]);
-			console.log('date', $scope.weeks.date);
 		}, true);
 	})
 	.directive('calNav', function(){
--- app/calendarDirectiveSpec.js
+++ app/calendarDirectiveSpec.js
@@ -18,10 +18,14 @@ describe('correctDate', function(){
 		$scope.date.day = 1;
 		$scope.date.year = 2016;
 		$scope.$apply();
-		console.log($scope.today);
+		console.log('scope.date',$scope.date);
+		console.log('scope.weeks[0][5]', $scope.weeks[0][5]);
+
 	}));
-	fit('should be the 5th element in array', function(){
-		expect($scope.date).toBe($scope.weeks[0][5]);
+	it('should be the 5th element in array', function(){
+		expect($scope.date.day).toBe($scope.weeks[0][5].day);
+		expect($scope.date.month).toBe($scope.weeks[0][5].month);
+		expect($scope.date.year).toBe($scope.weeks[0][5].year);
 	});
 });
 /*step 3 write a test where if the month and year a january 2016 
@@ -58,4 +62,50 @@ describe('how many months in drop down', function(){
 /*step 5 test to see how many years and if it is 20 years 
   ahead and before*/
 
-/*step 6 test directive*/
+describe('what year 20 years before and after today date', function(){
+	var $scope, ctrl;
+	beforeEach(module("calendarDemoApp"));
+	beforeEach(inject(function($controller, $rootScope){
+		$scope = $rootScope.$new();
+		ctrl = $controller('MonthCtrl', {
+			$scope: $scope
+		});
+	}));
+	fit('should be 1996 and 2036', function(){
+		expect($scope.date.year + 20).toEqual(2036);
+		expect($scope.date.year - 20).toEqual(1996);
+		expect($scope.years[0]).toBe(1996);
+		expect($scope.years[40]).toBe(2036);
+	});
+}); 
+
+/*	step 6 test directive: Remember though, this isn't actually testing the directive
+	You'll still want to `$compile` the directive, then using the `element` you can query 
+	and check that the various `td`s that are created have the appropriate classes*/
+describe('calNav', function(){
+	var $scope, scope, element, compiled, html;
+	beforeEach(module('calendarDemoApp'));
+	beforeEach(module('./app/calNav.html'));
+	beforeEach(inject(function($rootScope, $compile){
+		$scope = $rootScope.$new();
+		html ='<table>' + 
+					'<tr>' +
+						'<th ng-repeat="day in days" class="text-center">' +
+							'{{day}}' +
+						'</th>' +
+					'</tr>' +
+					'<tr ng-repeat = "week in weeks">' +
+						'<td ng-repeat="day in week" ng-class="{bookend: date.month != day.month,'+
+						' weekend: !day.weekday, weekday: day.weekday, today === day.date.toDateString()}"' + 
+							'{{day.day}}' +
+						'</td>' +
+					'</tr>' +
+				'<table>';
+		element = $compile(html)($scope);
+		console.log('html',html);
+		$rootScope.$digest();		
+	}));
+	it('should check <td> to have appropriate classes', function(){
+		expect(element.find('td')[5][0]).hasClass('today');
+	});
+});
