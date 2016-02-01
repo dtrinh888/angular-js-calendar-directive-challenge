f08005fcb54693e1c112fdd61b732f1f37581a3f
dtrinh888 <dtrinh888@gmail.com>
2016-01-31 16:25:14 -0800

    unit testing

--- app/app.js
+++ app/app.js
@@ -2,16 +2,19 @@ angular.module('calendarDemoApp', [])
 	.controller('MonthCtrl', function($scope){
 		
 		var today = new Date();
+		var day = today.getDay();
 		var month = today.getMonth();
 		var year = today.getFullYear();
-
+		console.log('day', day);
 		$scope.date = {
+			day: day,
 			month: month,
 			year: year
 		};
+		/*console.log('??',$scope.date);*/
 
 		$scope.today = today.toDateString();
-		console.log('today', $scope.today);
+		/*console.log('today', $scope.today);*/
 
 		$scope.days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
 						'Thursday', 'Friday', 'Saturday'];
@@ -30,21 +33,23 @@ angular.module('calendarDemoApp', [])
 		$scope.$watch('date', function(newDate){
 			console.log('newdate', newDate);
 			var range = CalendarRange.getMonthlyRange(new Date(newDate.year, newDate.month, 1));
-			console.log('range',range);
+			/*console.log('range',range);*/
 			var totalWeeks = range.days.length/7;			
 			var weeks = [];
 			for(i=0; i<totalWeeks; i++){
 				weeks[i] = [];
-				console.log('weeks', weeks);
-				console.log('totalWeeks', totalWeeks);
+				/*console.log('weeks', weeks[5]);*/
+				/*console.log('totalWeeks', totalWeeks);*/
 				for(j=0; j<7; j++){
-					console.log('days', j+(i*7));
+					/*console.log('days', j+(i*7));*/
 					weeks[i].push(range.days[j+(i*7)]);
-					console.log('j',j);
-					console.log('weeks[i]', weeks[i]);
+					/*console.log('j',j);
+					console.log('weeks[i]', weeks[0]);*/
 				}
 			}
 			$scope.weeks = weeks;
+			console.log('scope.weeks',$scope.weeks[0][5]);
+			console.log('date', $scope.weeks.date);
 		}, true);
 	})
 	.directive('calNav', function(){
--- app/calendarDirectiveSpec.js
+++ app/calendarDirectiveSpec.js
@@ -6,12 +6,56 @@ describe('test', function(){
 });
 /*step 2 write a test if today is january 1, 2016 then the 5th 
   element is today*/
-
+describe('correctDate', function(){
+	var $scope, ctrl;
+	beforeEach(module("calendarDemoApp"));
+	beforeEach(inject(function($controller, $rootScope){
+		$scope = $rootScope.$new();
+		ctrl = $controller('MonthCtrl', {
+			$scope: $scope
+		});
+		$scope.date.month = 0;
+		$scope.date.day = 1;
+		$scope.date.year = 2016;
+		$scope.$apply();
+		console.log($scope.today);
+	}));
+	fit('should be the 5th element in array', function(){
+		expect($scope.date).toBe($scope.weeks[0][5]);
+	});
+});
 /*step 3 write a test where if the month and year a january 2016 
   there should be number of bookend, weekday, weekends can be 
   written in one test with multilple expects*/
+describe('totalBookends', function(){
+	var $scope, ctrl;
+	beforeEach(module("calendarDemoApp"));
+	beforeEach(inject(function($controller, $rootScope){
+		$scope = $rootScope.$new();
+		ctrl = $controller('MonthCtrl', {
+			$scope: $scope
+		});
+		totalBookends = function(){
+
+		};
+	}));
+});
 
 /*step 4 test to see how many months are in the drop down*/
-
+describe('how many months in drop down', function(){
+	var $scope, ctrl;
+	beforeEach(module("calendarDemoApp"));
+	beforeEach(inject(function($controller, $rootScope){
+		$scope = $rootScope.$new();
+		ctrl = $controller('MonthCtrl', {
+			$scope: $scope
+		});
+	}));
+	it('should be equal to 12 months', function(){
+		expect($scope.months.length).toEqual(12);
+	});
+});
 /*step 5 test to see how many years and if it is 20 years 
   ahead and before*/
+
+/*step 6 test directive*/
